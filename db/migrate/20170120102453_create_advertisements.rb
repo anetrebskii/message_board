2ef50d16522ca0b4931c297f9e0ad4f80768e91e
Alexandr Netrebsky <alex.netrebsky@castle.co>
2017-01-20 16:28:40 +0400

    First implementation

---
+++ db/migrate/20170120102453_create_advertisements.rb
@@ -0,0 +1,12 @@
+class CreateAdvertisements < ActiveRecord::Migration[5.0]
+  def change
+    create_table :advertisements do |t|
+      t.text :title
+      t.text :description
+      t.text :phone
+      t.float :price, precise: 2
+
+      t.timestamps
+    end
+  end
+end
